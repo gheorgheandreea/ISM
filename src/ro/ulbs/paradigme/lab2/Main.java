38ed9747b68fc162e339339f2792e7b6767bc3c0
gheorghe <andreeamariana.gheorghe@ulbsibiu.ro>
2025-03-07 10:55:46 +0200

    Laborator2

---
+++ src/ro/ulbs/paradigme/lab2/Main.java
@@ -0,0 +1,6 @@
+package ro.ulbs.paradigme.lab2;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ro/ulbs/paradigme/lab2/Square.java
@@ -0,0 +1,23 @@
+package ro.ulbs.paradigme.lab2;
+
+public class Square extends Form {
+    private float side;
+    public Square(){
+        super();
+        this.side=0;
+    }
+    public Square(String color,float side) {
+        super(color);
+        this.side = side;
+    }
+    @Override
+    public float getArea(){
+        return side*side;
+    }
+    @Override
+    public String toString(){
+        return super.toString()+" And square area is "+getArea();
+    }
+
+
+}
