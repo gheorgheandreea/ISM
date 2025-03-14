94bf7510349afbb03932c374624bfa3918e3087e
gheorgheandreea <andreeamariana.gheorghe@ulbsibiu.ro>
2025-03-14 20:25:38 +0200

    Laborator3 tema

--- src/ro/ulbs/paradigme/lab3/PasswordMaker.java
+++ src/ro/ulbs/paradigme/lab3/PasswordMaker.java
@@ -2,15 +2,27 @@ package ro.ulbs.paradigme.lab3;
 
 import java.util.Random;
 
-public class PasswordMaker {
+ class PasswordMaker {
     private static final int MAGIC_NUMBER = new Random().nextInt(6) +5; ;
     private final String magicString;
     private final String name;
+    private static PasswordMaker instance = null;
+    private static int accessCount = 0;
+     private PasswordMaker() {
+         this.magicString = StringRandomizer.getRandomString(20);
+         this.name = " ";
+     }
     public PasswordMaker(String name) {
         this.name = name;
         this.magicString = StringRandomizer.getRandomString(20);
     }
-
+     public static PasswordMaker getInstance() {
+         if (instance == null) {
+             instance = new PasswordMaker();
+         }
+         accessCount++;
+         return instance;
+     }
         public String getPassword () {
             Random random = new Random();
             String s1 = StringRandomizer.getRandomString(MAGIC_NUMBER);
@@ -23,4 +35,7 @@ public class PasswordMaker {
             int s4 = random.nextInt(51);
             return s1 + s2 + s3 + s4;
         }
+     public static int getAccessCount() {
+         return accessCount;
+     }
     }
